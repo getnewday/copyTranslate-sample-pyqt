e19feeb8a92780cb829f3b3bb847bd18a12d981b
tla <756459364@qq.com>
2020-12-25 19:43:15 +0800

    add utils.py add readme.md

---
+++ readme.md
@@ -0,0 +1,4 @@
+## copyTranslator developmented by pyqt5 
+### TODO
+1. rewrite google translate interface. 
+2. implement drag copy function.
---
+++ utils.py
@@ -0,0 +1,10 @@
+class Utils:
+    def __init__(self):
+        pass
+
+    def processPlainText(self, text):
+        text = text.replace('\n', ' ')
+        print(text)
+        return text
+
+
